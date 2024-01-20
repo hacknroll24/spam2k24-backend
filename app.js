1bb33939dd0564e211fbb9b2bd337a43fe23f05d
Bryann Yeap Kok Keong <bryannyeapkk@gmail.com>
2024-01-20 22:52:55 +0800

    websockets

--- app.js
+++ app.js
@@ -2,9 +2,12 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
-const port = process.env.API_PORT;
+const API_PORT = process.env.API_PORT;
+const FRONTEND_PORT = process.env.FRONTEND_PORT;
 
 app.use(cors());
 
@@ -12,9 +15,74 @@ app.get("/", (req, res) => {
   res.send("Hello, Express!");
 });
 
-const apiRoutes = require("./routes/index");
-app.use("/api", apiRoutes);
+// const apiRoutes = require("./routes");
+// app.use("/api", apiRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: `http://localhost:${FRONTEND_PORT}`,
+  },
+});
+
+// Store the mapping of socket IDs to usernames
+const socketToUsername = {};
+function getUsersInRoom(roomCode) {
+  const socketIdsInRoom = Array.from(
+    io.sockets.adapter.rooms.get(roomCode) || []
+  );
+  const usersInRoom = socketIdsInRoom.map((socketId) => ({
+    [socketId]: socketToUsername[socketId],
+  }));
+  return socketToUsername;
+}
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // socket.join(roomCode);
+  // socket.emit("currentPlayers", io.in(roomCode).clients);
+
+  // Handle events from the client
+  socket.on("sendMessage", (message) => {
+    console.log("Received message from client:", message);
+
+    // Broadcast the message to all connected clients
+    io.emit("message", { text: message });
+  });
+
+  socket.on("joinRoom", ({ roomCode, user }) => {
+    socket.join(roomCode);
+
+    // Messages
+    socket.emit("message", `You joined room ${roomCode}`);
+    socket.to(roomCode).emit("message", `${user} joined room ${roomCode}`);
+
+    console.log(io.sockets.adapter.rooms.get(roomCode));
+
+    socket.emit("roomJoined");
+
+    socketToUsername[socket.id] = user;
+
+    io.to(roomCode).emit("userList", getUsersInRoom(roomCode));
+  });
+
+  socket.on("click", (newIq, user) => {
+    socket.to(roomCode).emit("updateClick", { user, newIq });
+    socket.to(roomCode).emit("message", { user, newIq });
+  });
+
+  socket.on("accountUser", (user) => {
+    socket.emit("accountUser", user);
+  });
+
+  // Handle disconnect
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+    delete socketToUsername[socket.id];
+  });
+});
+
+server.listen(API_PORT, () => {
+  console.log(`Socket.IO Server is running on http://localhost:${API_PORT}`);
 });
